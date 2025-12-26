c87d8fa17206eb7fcde00d724249c19a7394ce0d
Sahil0912 <sahiljakhar.09122005@gmail.com>
2025-12-27 00:47:07 +0530

    Made the testing more readable and slightly optimised

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,16 @@ int main(){
 
     OrderBook testOrderBook;
     MatchingEngine testEngine;
-    Order order1(1, 100, Side::Sell, OrderType::Limit, 100);
-    std::shared_ptr<Order> order_ptr1 = std::make_shared<Order>(order1);
+    // Order order1(1, 100, Side::Sell, OrderType::Limit, 100);
+    // std::shared_ptr<Order> order_ptr1 = std::make_shared<Order>(order1);
+    auto order_ptr1 = std::make_shared<Order>(1, 100, Side::Sell, OrderType::Limit, 100);
     testEngine.ProcessOrder(order_ptr1, testOrderBook);
 
     testOrderBook.PrintOrderBook();
     
-    Order order2(2, 50, Side::Buy, OrderType::Market);
-    std::shared_ptr<Order> order_ptr2 = std::make_shared<Order>(order2);
+    // Order order2(2, 50, Side::Buy, OrderType::Market);
+    // std::shared_ptr<Order> order_ptr2 = std::make_shared<Order>(order2);
+    auto order_ptr2 = std::make_shared<Order>(2, 50, Side::Buy, OrderType::Market);
     testEngine.ProcessOrder(order_ptr2, testOrderBook);
     
     testOrderBook.PrintOrderBook();
