ca48fa31938a2e053c7c26b009df29d1a84dd4ab
Sahil0912 <sahiljakhar.09122005@gmail.com>
2026-04-28 00:46:07 +0530

    Added remove order functionality

--- src/order_book/price_level.hpp
+++ src/order_book/price_level.hpp
@@ -7,10 +7,13 @@ class PriceLevel{
         Order* head;
         Order* tail; // for appending the order at last (FIFO)
         int orderCount;
-    public : 
-        explicit PriceLevel(Price _price) : price(_price), head(nullptr), tail(nullptr), orderCount(0) {}
+    public :
+        PriceLevel() : price(0), head(nullptr), tail(nullptr), orderCount(0) {} 
+        PriceLevel(Price _price) : price(_price), head(nullptr), tail(nullptr), orderCount(0) {}
+
         Price GetPrice() const{ return price; }
         bool IsEmpty() const{ return (orderCount == 0); }
+
         void AddOrder(Order* order) {
             if(!head){
                 head = tail = order;
@@ -30,7 +33,29 @@ class PriceLevel{
             if(head) head->prev = nullptr;
             else tail = nullptr;
         }
+
         Order* GetHead() const { return head; }
+        void RemoveOrder(Order* order){
+            
+            if(order == head && order == tail){
+                head = tail = nullptr;
+            }
+            else if(order == head){
+                head = head->next;
+                head->prev = nullptr;
+            }
+            else if(order == tail){
+                tail = tail->prev;
+                tail->next = nullptr;
+            }
+            else{
+                auto ptr_prev = order->prev;
+                auto ptr_next = order->next;
+                ptr_prev->next = ptr_next;
+                ptr_next->prev = ptr_prev;
+            }
+            orderCount--;
+        }
         void PrintPriceLevel();
         
 
